thorough revision of regrid_obs.py and aggregate_hindcast.py concering the treatment of the variable units; multiple errors were corrected so that the package now should be ready for validating t2m, tp, si10 and and ssrd; plot_seasonal_validation_results.py now produces a single netCDF file containing the binary skill masks; get_skill_season.py now calclates the bias and relative bias; slight modifications in functions_seasonal.py

aggregate_hindcast.py
@@ -19,14 +19,13 @@ import cartopy.feature as cf
 #exec(open('functions_seasonal.py').read()) #reads the <functions_seasonal.py> script containing a set of custom functions needed here
 
 ##set input parameters for observational datasets to be regridded
-#variables = ['hurs','pr','psl','rsds','sfcWind','tas','tasmax','tasmin'] #variable names in directories and file names
+#variables = ['hurs','pr','psl','rsds','sfcWind','tas','tasmax','tasmin'] #variable names in directories and file names, note that pr and rsds are initially fluxes accumulated over the entire forecast period; these variables are dis-aggregeated to daily accumulations below in this script.
 #variables_nc = ['hurs','pr','msi','rsds','sfcWind','tas','tasmax','tasmin'] #variable names within the netCDF files
 
-variables = ['tas','pr'] #variable names in directories and file names
-variables_nc = ['tas','pr'] #variable names within the netCDF files
-variables_new = ['t2m','tp'] #new variable names; as provided by ERA5 data from CDS
+variables = ['sfcWind','tas','pr','rsds'] #variable names in directories and file names
+variables_nc = ['sfcWind','tas','pr','rsds'] #variable names within the netCDF files
+variables_new = ['si10','t2m','tp','ssrd'] #new variable names; as provided by ERA5 data from CDS
 
-#years = [1981,1982]
 #years = [1981,2016] #years to be regridded
 years = [1981,2022]
 
@@ -74,8 +73,22 @@ for mm in np.arange(len(model)):
                     continue
                 
                 if (variables[vv] == 'tas') & (model[mm] == 'ecmwf') & (version[mm] == '51'):
-                    print('Info: Adding 273.15 Kelvin to '+variables[vv]+' data from '+model[mm]+version[mm]+' to correct Predictia workflow error.')
-                    nc[variables[vv]].values = nc[variables[vv]].values+273.15
+                    print('Info: Adding 2x273.15 to '+variables[vv]+' data from '+model[mm]+version[mm]+' to correct Predictia workflow error and then transform into Kelvin.')
+                    #nc[variables[vv]].values = nc[variables[vv]].values+273.15
+                    nc[variables[vv]][:] = nc[variables[vv]]+273.15+273.15
+                    nc[variables[vv]].attrs['units'] = 'daily mean '+variables_new[vv]+' in Kelvin'
+                elif (variables[vv] in ('pr','rsds')) & (model[mm] == 'ecmwf') & (version[mm] == '51'):
+                    print('Info: Disaggregate '+variables[vv]+' accumulated over the '+str(len(nc.time))+' days forecast period from '+model[mm]+version[mm]+' to daily sums.')
+                    vals_disagg = np.diff(nc[variables[vv]].values,n=1,axis=0)
+                    shape_disagg = vals_disagg.shape
+                    add_day = np.expand_dims(vals_disagg[-1,:,:,:],axis=0) #get last available difference
+                    #add_day = np.zeros((1,shape_disagg[1],shape_disagg[2],shape_disagg[3]))
+                    #add_day[:] = np.nan
+                    vals_disagg = np.concatenate((vals_disagg,add_day),axis=0)
+                    nc[variables[vv]][:] = vals_disagg
+                    nc[variables[vv]].attrs['units'] = 'daily accumulated '+variables_new[vv]+' in '+nc[variables[vv]].attrs['units']
+                else:
+                    print('Info: No data transformation is applied for '+variables[vv]+' data from '+model[mm]+version[mm]+'.')
                 
                 ##select ensemble members. This is done because the ensemble members in the forecast period may be more than in the hindcast period, e.g. 25 vs. 50 in ECWMF 51
                 #print('INFO: Selecting the first '+str(nr_mem[mm])+' ensemble members from a total of '+str(nc.member.shape[0])+' members...') 
@@ -103,7 +116,7 @@ for mm in np.arange(len(model)):
                 data_mon[pos_time,pos_lead,:,:,:] = nc_mon.values
 
                 nc.close()
-                nc_mon.close()                
+                nc_mon.close()
                 del(nc,nc_mon)
                 print(pos_time)
         

functions_seasonal.py
@@ -65,7 +65,7 @@ def lin_detrend(xr_ar):
     xr_ar_detrended = xr_ar - fit
     return(xr_ar_detrended)
 
-def get_fraq_significance(np_arr_pval_f,np_arr_rho_f,critval_f):
+def get_frac_significance(np_arr_pval_f,np_arr_rho_f,critval_f):
     """get the fraction of grid-boxes where significant results are obtained in percentage of all grid-boxes forming the domain; np_arr_f is a 4d numpy array
     with the dimensions season x lead x lat x lon"""
     shape_f = np_arr_pval_f.shape
@@ -78,6 +78,18 @@ def get_fraq_significance(np_arr_pval_f,np_arr_rho_f,critval_f):
     np_arr_pval_step_f[spurind_f] = 0
     spatial_sigfraq_f = np.sum(np_arr_pval_step_f,axis=2)/(shape_f[2]*shape_f[3])*100
     return(spatial_sigfraq_f,np_arr_pval_step_f)
+
+def get_frac_above_threshold(np_arr_vals_f,critval_f):
+    """get the fraction of grid-boxes where values i values_f exceed the threshold <critval_f>; np_arr_f is a 4d numpy array with the dimensions season x lead x lat x lon"""
+    shape_f = np_arr_vals_f.shape
+    np_arr_vals_step_f = np.reshape(np_arr_vals_f,[shape_f[0],shape_f[1],shape_f[2]*shape_f[3]])
+    sigind_f = np_arr_vals_step_f > critval_f 
+    spurind_f = np_arr_vals_step_f <= critval_f
+    #spurind_f = sigind_f == False
+    np_arr_vals_step_f[sigind_f] = 1
+    np_arr_vals_step_f[spurind_f] = 0
+    spatial_fraq_f = np.sum(np_arr_vals_step_f,axis=2)/(shape_f[2]*shape_f[3])*100
+    return(spatial_fraq_f,np_arr_vals_step_f)
 
 def plot_pcolormesh_seasonal(xr_ar_f,minval_f,maxval_f,savename_f,colormap_f,dpival_f):
     '''Plots matrix of the verfication results contained in xarray data array <xr_ar_f>, seasons are plotted on the x axis, lead months on the y axis.'''
@@ -130,7 +142,7 @@ def get_map_lowfreq_var(pattern_f,xx_f,yy_f,agree_ind_f,minval_f,maxval_f,dpival
 
     ax.plot(toplayer_x, toplayer_y, color='blue', marker=marker_f, linestyle='none', markersize=pointsize_f, transform=ccrs.PlateCarree(), zorder=4)
     if origpoint != None:
-        ax.plot(origpoint[0], origpoint[1], color='blue', marker='X', linestyle='none', markersize=2, transform=ccrs.PlateCarree(), zorder=5)        
+        ax.plot(origpoint[0], origpoint[1], color='blue', marker='X', linestyle='none', markersize=2, transform=ccrs.PlateCarree(), zorder=5)
     ##plot parallels and meridians
     #gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, linewidth=0.5, color='blue', alpha=0.5, linestyle='dotted', zorder=6)
     #gl.xformatter = LONGITUDE_FORMATTER

regrid_obs.py
@@ -19,7 +19,8 @@ obs = ['era5'] #name of the observational / reanalysis dataset that will be regr
 agg_src = ['mon'] #temporal aggregation of the observational input files, pertains to the <obs> loop indicated with <oo> below
 startyear_file = [1940] #start year of the obs file as indicated in filename
 endyear_file = [2022] #corresponding end year
-variables = ['t2m','tp'] #variables to be regridded
+variables = ['si10','ssrd','t2m','tp'] #variables to be regridded
+#variables = ['t2m'] #variables to be regridded
 years = [1981,2022] #years to be regridded
 int_method = 'conservative_normed' #'conservative_normed', interpolation method used by xesmf
 
@@ -71,10 +72,25 @@ for oo in np.arange(len(obs)):
             obs_dates = pd.DatetimeIndex(nc_obs_data.time.values)
             yearbool = (obs_dates.year >= years[0]) & (obs_dates.year <= years[1])
             nc_obs_data = nc_obs_data.isel(time=yearbool)
+            obs_dates = obs_dates[yearbool]
             #transform the data, depending on the input dataset and variable
             if obs[oo] == 'era5' and variables[vv] == 'tp':
-                print('INFO: multiplying raw '+variables[vv]+' values from '+obs[oo]+' with the factor 86400 !')
-                nc_obs_data[variables[vv]][:] = nc_obs_data[variables[vv]]*86400
+                print('INFO: multiply raw '+variables[vv]+' values from '+obs[oo]+' by 1000 to bring units from '+nc_obs_data[variables[vv]].units+' to mm/day!')
+                nc_obs_data[variables[vv]][:] = nc_obs_data[variables[vv]]*1000 #transform ERA5 pr data from metre/day to mm/day
+                nc_obs_data[variables[vv]].attrs['units'] = 'mm/day'
+            elif obs[oo] == 'era5' and variables[vv] in ('ssrd'):
+                print('INFO: Divide raw '+variables[vv]+' values from '+obs[oo]+' by the number of seconds per month to bring units from '+nc_obs_data[variables[vv]].units+' accumulated over the month to W/m2!')
+                days_in_month = obs_dates.days_in_month.values
+                obs_shape = nc_obs_data[variables[vv]].shape
+                ##<days in month> is most probably not needed because the raw ssrd values from ERA5 are most probably daily accumulated fluxes in W/m2 and therefore just need to be divided by 86400, see below
+                #days_in_month = np.expand_dims(np.expand_dims(days_in_month,axis=-1),axis=-1)
+                #days_in_month = np.tile(days_in_month,(1,obs_shape[1],obs_shape[2]))
+                #nc_obs_data[variables[vv]][:] = nc_obs_data[variables[vv]]/(days_in_month*86400)
+                nc_obs_data[variables[vv]][:] = nc_obs_data[variables[vv]]/86400
+                nc_obs_data[variables[vv]].attrs['units'] = 'W/m2'
+            else:
+                print('INFO: '+variables[vv]+' from '+obs[oo]+' comes in '+nc_obs_data[variables[vv]].units+' and no transformation is applied.')
+                nc_obs_data[variables[vv]].attrs['units'] = nc_obs_data[variables[vv]].units
             
             #regrid without mask
             print('INFO: Regridding '+variables[vv]+' from '+obs[oo]+' on '+model[mm]+' '+version[mm]+' grid using '+int_method+' method from xesmf...')